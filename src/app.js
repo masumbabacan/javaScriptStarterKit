a4b9f5ddce16adafb285bf42e2b7d36ff98d0d8c
masumbabacan <78920463+masumbabacan@users.noreply.github.com>
2021-10-18 23:51:10 +0300

    js ders 1

---
+++ src/app.js
@@ -0,0 +1,10 @@
+console.log("merhaba kodlama io");
+
+let konutKredileri = ["konut 1","konut 2","konut 3","konut 4","konut 5"]
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+
+console.log(konutKredileri)
